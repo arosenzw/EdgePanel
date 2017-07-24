c5e14171d152ec7a9fc467659d9b2cbaed2a55f8
Devon Powell <devon.powell@etrade.com>
2017-07-24 15:31:14 -0400

    Activity now updates edge panel when called

--- EdgePanelApp/src/main/java/com/etrade/edgepanel/EdgeConfigure.java
+++ EdgePanelApp/src/main/java/com/etrade/edgepanel/EdgeConfigure.java
@@ -5,11 +5,19 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 import com.etrade.edgepanel.edgedisplay.WidgetProvider;
 import com.samsung.android.sdk.look.cocktailbar.SlookCocktailManager;
 
 public class EdgeConfigure extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshEdgePanel(this);
+    }
 
     /**
      * Allows the main app activity update the edge panel
